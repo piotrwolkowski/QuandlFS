408f3ff6ddf708f2ad085f3b65e1bd9f845762fe
piotrwolkowski <piotr.wolkowski@gmail.com>
2015-09-08 23:55:53 -0300

    Added some comments.

--- QuandlFS/QuandlTypes.fs
+++ QuandlFS/QuandlTypes.fs
@@ -5,66 +5,119 @@ module QuandlTypes
     open FSharp.Core
     open FSharp.Data
     
+    /// The interface indicating the request without constraints.
     type INoConstraintsRequest = interface end
+
+    /// The interface indicating an request.
     type IRequest= interface end
 
+    /// When the frequency of a dataset is changed, Quandl returns 
+    /// the last observation for the given period. So, if you collapse 
+    /// a daily dataset to monthly, you will get a sample of the original 
+    /// dataset where the observation for each month is the last data point 
+    /// available for that month. Thus this transformation does not work 
+    /// well for datasets that measure percentage changes, period averages 
+    /// or period extremes (highs and lows).
     type FrequencyEnum =
+        /// No frequency
         | None = 1
+        
+        /// Daily values
         | Daily = 2
+        
+        /// Weekly values
         | Weekly = 4
+
+        /// Monthly values
         | Monthly = 8
+
+        /// Quarterly values
         | Quarterly = 16
+
+        /// Annual values
         | Annual = 32
 
+    /// The format code to request the Quandl data in specified format.
     type DataFormatEnum =
+        /// The data in CSV format
         | CSV = 1
+
+        /// The data in JSON format
         | JSON = 2
+
+        /// The data in XML format
         | XML = 4
 
+    /// The sort order of the data. Ascending by default.
     type SortOrderEnum =
+        /// The data in ascending order
         | Ascending = 1
+
+        /// The data in descending order
         | Descending = 2
 
+    /// Quandl allows to perform certain elementary calculations on the data prior to downloading. 
+    /// The transformations currently available are row-on-row change, percentage change, 
+    /// cumulative sum, and normalize (set starting value at 100).
     type CalculationsEnum =
+        /// Row-on-row change. y'[t] = y[t] - y[t-1] where y[t] denotes a datapoint for time
+        /// and y'[t] denotes transformed data
         | Diff = 1
+
+        /// Row-on-row percent change. y'[t] = (y[t] - y[t-1])/y[t-1] where y[t] denotes 
+        /// a datapoint for time and y'[t] denotes transformed data
         | RDiff = 2
+
+        /// Cumulative sum. y'[t] = y[t] +y[t-1] + ... + y[0] where y[t] denotes 
+        /// a datapoint for time and y'[t] denotes transformed data
         | Cumul = 4
+
+        /// Normalize (set starting value at 100). y'[t] = (y[t]/y[0]) * 100 where y[t] denotes 
+        /// a datapoint for time and y'[t] denotes transformed data
         | Normalize = 8
 
+    /// The record determining the data base and the data set.
     type DataRequest =
         {
+            // The data base name as available from Quandl.
             DataBase:string
+
+            // The data set name as available from Quandl.
             DataSet:string
         }
 
+    /// The record determining the data format and data sort order.
     type DataFormatting = 
         {
+            /// The data format
             DataFormat:DataFormatEnum
+
+            /// The data sort order
             SortOrder:SortOrderEnum option
         }
 
-    // Set any expected constraint. If it's not set it will be ignored.
+    /// The record determining additional data constraints.
     type Constraints =
         {
-            // do not include data column names
+            /// Excludes the headers
             ExcludeColumnNames:bool option
 
-            // get n first rows
+            /// Get n first rows
             Truncate:int option
         
-            // set a start date
+            /// Set a start date
             StartDate:DateTime option
         
-            // set an end date
+            /// Set an end date
             EndDate:DateTime option
         
-            // get specific column
+            /// Get specified column
             ColumnIndex:int option
         
-            // set data frequency
+            /// Set data frequency
             Frequency:FrequencyEnum option
         
-            // set expected calculations
+            /// Set expected calculations
             Calculations:CalculationsEnum option
         }
 
